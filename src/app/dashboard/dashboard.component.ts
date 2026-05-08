287deb258ed143093132a063d95b00e5437d3e34
saminathsankar <dev.saminath@gmail.com>
2026-05-08 09:19:34 +0530

    Dashboard_Changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,10 +24,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    if (!isDevMode()) {
-      this.router.navigate(['/']);
-      return;
-    }
     this.liveCount = this.userCountService.currentCount;
     this.displayedCount = this.liveCount;
     this.sub = this.userCountService.count$.subscribe(count => {
